bbc35959645979b8cc228e114a379b78b624d1b4
Melvin Carvalho <https://melvincarvalho.com/#me>
2022-07-11 14:53:50 +0200

    add ping interval to updater

--- bin/updater.sh
+++ bin/updater.sh
@@ -2,7 +2,7 @@
 
 RELAY=${1:-ws://nostr.rocks:1617}
 
-echo "[\"REQ\", \"tail\", { \"since\": $(date +%s) }]" | websocat -n $RELAY | while read -r line
+echo "[\"REQ\", \"tail\", { \"since\": $(date +%s) }]" | websocat --ping-interval 60 -n $RELAY | while read -r line
 do 
   echo $line 
   git pull
